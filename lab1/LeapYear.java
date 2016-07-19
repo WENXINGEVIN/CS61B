fbf15b6899eb97639608c937f84d9bb101a2edca
WENXINGEVIN <VINCENTWEN@MacBook-Pro.local>
2016-07-19 12:07:33 +0800

    Completed LeapYear.java

---
+++ lab1/LeapYear.java
@@ -0,0 +1,23 @@
+public class LeapYear{
+	int year;
+
+	public LeapYear(int i) {
+		year=i;
+	}
+
+	public boolean checkLeapYear(){
+		if(year%400==0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		LeapYear aYear=new LeapYear(2014);
+		System.out.println(aYear);
+		System.out.println(aYear.checkLeapYear());
+		LeapYear anotherYear=new LeapYear(2000);
+		System.out.println(anotherYear.checkLeapYear());
+	}
+	
